7faad8287663f53dc126c82353cb294b8a33d864
nypunyagowda <nypunyagowda20@gmail.com>
2026-01-09 15:56:35 +0530

    practice

---
+++ Assignments Python/Simulation + Analytics/countries.py
@@ -0,0 +1,10 @@
+countries = ["America","canada","Australia","china","chile"]
+for c in countries:
+    if c.startswith("c"):
+        countries.remove(c)
+print(countries)
+for c in countries:
+    if c.startswith("c"):
+        countries.remove(c)
+print(countries)
+    
