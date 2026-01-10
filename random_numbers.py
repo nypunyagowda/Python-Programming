73e490a56562c74b38f351bd7923cc045c096cb2
nypunyagowda <nypunyagowda20@gmail.com>
2026-01-10 23:47:50 +0530

    random number generator

---
+++ random_numbers.py
@@ -0,0 +1,10 @@
+import random
+
+# Input range
+start = int(input("Enter the starting number: "))
+end = int(input("Enter the ending number: "))
+
+# Generate random number
+random_number = random.randint(start, end)
+
+print("Random number:", random_number)
