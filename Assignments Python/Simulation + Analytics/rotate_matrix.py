2fff2b58edc943c5356db28be7aa3ae5d4829385
nypunyagowda <nypunyagowda20@gmail.com>
2026-01-12 00:27:11 +0530

    first commit

--- Assignments Python/Simulation + Analytics/rotate_matrix.py
+++ Assignments Python/Simulation + Analytics/rotate_matrix.py
@@ -1,11 +1,22 @@
 def rotate_matrix(mat):
     n = len(mat)
 
-    # Step 1: Transpose the matrix
+    # transpose
     for i in range(n - 1):
         for j in range(i + 1, n):
             mat[i][j], mat[j][i] = mat[j][i], mat[i][j]
 
-    # Step 2: Reverse each row
+    # reverse each row
     for i in range(n):
         mat[i].reverse()
+
+
+mat = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+rotate_matrix(mat)
+print(mat)
+
