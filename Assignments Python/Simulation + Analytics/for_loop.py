f709676fe8e13940a3a624eb72b935fb5fea3095
nypunyagowda <nypunyagowda20@gmail.com>
2026-01-20 14:05:03 +0530

    first commit

--- Assignments Python/Simulation + Analytics/for_loop.py
+++ Assignments Python/Simulation + Analytics/for_loop.py
@@ -1,22 +1,6 @@
-name = "PythonProgramming"
+name = "Vaibhav"
 for index,letters in enumerate(name):
-    print(letters*(index+1))'''
-
-#example 2
-
-cities = ["blr","ckm","mlr","mys"]
-for city in cities:
-    if city == "mlr":
-        print(f"found {city}!")
-        break
-    print(city)
-
-#example 3
-l = [12,27,87,89]
-for num in l:
-    print(num)
-else:
-    print("all printed")
+    print(letters*(index+1))
 
 
 
