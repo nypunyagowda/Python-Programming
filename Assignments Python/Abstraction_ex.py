23142d68f0209ea9501adf3f50a5ceb9adfecd85
nypunyagowda <nypunyagowda20@gmail.com>
2026-02-02 21:40:41 +0530

    First commit

---
+++ Assignments Python/Abstraction_ex.py
@@ -0,0 +1,11 @@
+class Car:
+    def Start(self):
+        print("Car engine started")
+    def Accelerate(self):
+        print("Car is moving")
+    def Break(self):
+        print("Car stopped")
+car = Car()
+car.Start()
+car.Accelerate()
+car.Break()
